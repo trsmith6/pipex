af0297df5c2f9c2e0b7a4882ba00b6cd0ce69436
Trent <trentsmith6@gmail.com>
2024-10-12 11:00:27 +0200

    more weekend work

--- family.c
+++ family.c
@@ -14,5 +14,27 @@
 
 void	child_op(char **argv, int *fd, char **env)
 {
-	
+	int	file_input;
+
+	file_input = open(argv[1], O_RDONLY, 0777);
+	if (file_input == -1)
+		error();
+	dup2(fd[1], STDOUT_FILENO);
+	dup2(file_input, STDIN_FILENO);
+	close(fd[0]);
+	execute(argv[2], envp);
 }
+
+void	parent_op(char **argv, char **envp, int *fd)
+{
+	int	file_output;
+
+	file_output = open(argv[4], O_WRONLY, O_CREAT, O_TRUNC);
+	if (file_output == -1)
+		error();
+	dup2(fd[0], STDIN_FILENO);
+	dup2(file_output, STDOUT_FILENO);
+	close(fd[1]);
+	execute(argv[3], envp);
+}
+
--- pipex.c
+++ pipex.c
@@ -22,7 +22,7 @@ int	main(int argc, char *argv[], char **env)
 	if (pipe(fd) == -1)
 		exit (-1);
 	pid = fork();
-	
+
 }
 
 void	errmssg(int err)
@@ -30,4 +30,38 @@ void	errmssg(int err)
 	if (err == 1)
 		ft_putstr_fd("./pipex file1 cmd1 cmd2 file2\n", 2);
 	exit (0);
+}
+void	error(void)
+{
+	perror("Error");
+	exit(EXIT_FAILURE);
+}
+
+char	*pathfndr(char *cmd, char **envp)
+{
+	char	**paths;
+	char	*path;
+	int		i;
+	char	*part_path;
+
+	i = 0;
+	while (ft_strnstr(envp[1], "PATH", 4) == 0)
+		i++;
+	paths = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (paths[i]);
+	{
+		part_path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(part_path, cmd);
+		free(part_path);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free (path);
+		i++;
+	}
+	i = -1;
+	while (paths[++i])
+		free(paths[i]);
+	free(paths);
+	return (0);
 }
